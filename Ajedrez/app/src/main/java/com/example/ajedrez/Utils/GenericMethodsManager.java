694ada7c00ad1ac23114857322ee6aaf71f55f97
sejolopab <jose.gamboa@avantica.net>
2019-04-09 22:33:55 -0600

    Load calendar on saved date

--- Ajedrez/app/src/main/java/com/example/ajedrez/Utils/GenericMethodsManager.java
+++ Ajedrez/app/src/main/java/com/example/ajedrez/Utils/GenericMethodsManager.java
@@ -5,12 +5,15 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class GenericMethodsManager {
     private static GenericMethodsManager instance = new GenericMethodsManager();
@@ -23,7 +26,8 @@ public class GenericMethodsManager {
     }
 
     public String getSimpleDate() {
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String stringFormat = "dd/MM/yyyy";
+        DateFormat dateFormat = new SimpleDateFormat(stringFormat);
         Date date = new Date();
         return dateFormat.format(date);
     }
@@ -33,6 +37,18 @@ public class GenericMethodsManager {
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        Editable text = appCompatEditText.getText();
+        if(text != null){
+            if (text.length() != 0) {
+                String date = text.toString();
+                List<String> splitDate = Arrays.asList(date.split("/"));
+                dayOfMonth = Integer.valueOf(splitDate.get(0));
+                month = Integer.valueOf(splitDate.get(1)) - 1;
+                year = Integer.valueOf(splitDate.get(2));
+            }
+        }
+
         DatePickerDialog datePickerDialog = new DatePickerDialog(context,
                 (datePicker, year1, month1, day) ->
                         appCompatEditText.setText(day + "/" + (month1 + 1) + "/" + year1), year, month, dayOfMonth);
